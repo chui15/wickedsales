e525e42d6bf9c5640e0c87961d0d86fee66d20d0
chui15 <cwsh15@gmail.com>
2019-10-07 23:06:02 +0000

    successfully retrieved data from database

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('db_connection.php');
+
 header('Content-Type: application/json');
 
 if (empty($_GET['id'])) {
@@ -7,5 +9,21 @@ if (empty($_GET['id'])) {
 } else {
   readfile('dummy-product-details.json');
 }
+
+$query = "SELECT * from `Products`";
+
+$result = mysqli_query($conn, $query);
+
+if(!$result){
+  throw new Exception('query error: ' . mysqli_error($conn));
+}
+
+$output = [];
+while($row = mysqli_fetch_assoc($result)){
+  $output[] = $row;
+}
+
+$jsonData = json_encode($output);
+print($jsonData);
 
 ?>
