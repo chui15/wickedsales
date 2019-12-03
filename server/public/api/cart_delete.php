5f13de2be4b7a1d698f37bd937c9bab7544fd63e
chui15 <cwsh15@gmail.com>
2019-12-03 22:55:27 +0000

    updated routing for nav bar

--- server/public/api/cart_delete.php
+++ server/public/api/cart_delete.php
@@ -1,18 +1,30 @@
 <?php
 
+define('INTERNAL', true);
 require_once('functions.php');
+set_exception_handler('error_handler');
+session_start();
+require_once('db_connection.php');
 
 $data = json_decode(file_get_contents('php://input'), 1);
 
 $id = intval($data);
 
-$query = "DELETE FROM `cartItems` WHERE `productID` = $id";
-
-$result = mysqli_query($conn, $query);
-
-
-if (!$result) {
-  throw new Exception("The connection failed or no data was received!",  mysqli_connect_error());
+if (empty($_SESSION['cartId'])) {
+  $data = json_encode([]);
+  exit();
 }
+
+$cartID = intval($_SESSION['cartId']);
+
+$deleteQuery = "DELETE FROM `cartItems` WHERE `productID` = $id && `cartID` = $cartID";
+
+$deleteResult = mysqli_query($conn, $deleteQuery);
+
+if (!$deleteResult) {
+  throw new Exception('delete query failed' . mysqli_connect_error());
+}
+
+print(json_encode($data));
 
 ?>
